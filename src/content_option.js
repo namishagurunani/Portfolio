a5abd9a901a40d643c14be1a7788cf0c2c535794
shivam gurunani <namisha.gurunani173261@gmail.com>
2024-04-25 00:19:17 +0530

    Project Completed

---
+++ src/content_option.js
@@ -0,0 +1,204 @@
+const logotext = "Namisha Gurunani";
+const meta = {
+    title: "Namisha Gurunani",
+    description: "I'm Namisha Gurunani, a Full Stack Developer based in Delhi, India, with a strong passion for coding. My journey in the world of development began with a simple project, and since then, I've been continuously learning and exploring various technologies to turn ideas into reality.",
+};
+
+const introdata = {
+    title: "I’m Namisha Gurunani",
+    animated: {
+        first: "I love coding",
+        second: "I develop cool websites",
+        third: "I create responsive websites",
+    },
+    description: "Hello, I'm Namisha Gurunani 👋🏻, a passionate and creative developer based in Delhi. I have a keen interest in building impactful solutions. My coding journey started with a straightforward project, and ever since, I've been on a continuous learning path, exploring various technologies and frameworks to transform concepts into reality.",
+    your_img_url: "https://i.postimg.cc/9Msy07sG/profile.jpg",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "I am a Full Stack Developer based in Delhi, India, specializing in frontend and backend development. I am proficient in JavaScript, React, HTML/CSS, Tailwind CSS, Bootstrap, MongoDB, and Node.js.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Full Stack Developer Intern",
+        where: "Geekster, Gurugram",
+        date: "2023 - 2024",
+    }
+];
+
+const skills = [{
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "JavaScript",
+        value: 85,
+    },
+    {
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "CSS",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Redux",
+        value: 75,
+    },
+    {
+        name: "Tailwind",
+        value: 75,
+    } ,
+    {
+        name: "Bootstrap",
+        value: 65,
+    },
+    {
+        name: "Node.js",
+        value: 75,
+    },
+    {
+        name: "Express.js",
+        value: 75,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+    {
+        name: "Firebase",
+        value: 85,
+    }
+];
+
+const services = [
+    {
+        title: "UI & UX Design",
+        description: "I specialize in creating engaging user interfaces and experiences.",
+    },
+    {
+        title: "Website Development",
+        description: "I design and develop responsive and user-friendly websites tailored to your needs.",
+    },
+    {
+        title: "Custom Web Applications",
+        description: "I develop bespoke web applications with a focus on scalability and performance.",
+    },
+];
+
+
+
+const dataportfolio = [
+    {
+        img: "https://i.postimg.cc/2jGm4wqK/Screenshot-2024-04-24-215010.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://react-google-drive.vercel.app/",
+    },
+    {
+        img: "https://images.ctfassets.net/4cd45et68cgf/637ArQo6YNsJf48Xw3zUAg/d030f9065a0fe54de777eaa600cf3fcb/N-Games_March_936x622.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://main--netflix.netlify.app/",
+    },
+    
+    {
+        img: "https://i.postimg.cc/15qzbxdt/Screenshot-2024-04-24-212622.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://geeks-food-routing.vercel.app/",
+    },
+    {
+        img: "https://i.postimg.cc/h4LBWSrf/Screenshot-2024-04-24-214441.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/JavascriptProjects/LeaderBoard/",
+    },
+    {
+        img: "https://i.postimg.cc/rFRn8yLN/Screenshot-2024-04-24-213039.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://react-directory-app.vercel.app/",
+    },
+    {
+        img: "https://r2.easyimg.io/7e6v8vuzo/screenshot_2024-04-24_193727.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://mayankkatheriya.github.io/Mct_4/Namisha_Payment-integration/",
+    },
+    
+    {
+        img: "https://i.postimg.cc/vTk0Wk5n/Screenshot-2024-04-24-213450.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://unsplash-project-two.vercel.app/",
+    },
+    
+    {
+        img: "https://i.postimg.cc/y6STWtR4/Screenshot-2024-04-24-215316.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/Pokemon/pokemon/",
+    },
+    {
+        img: "https://i.postimg.cc/k5SqKP8Q/Screenshot-2024-04-24-214927.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/JAVASCRIPT/Tip-Calculator/",
+    },
+    {
+        img: "https://i.postimg.cc/mrYKTLC5/Screenshot-2024-04-24-214702.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/Javascriptweek-6/GitHub%20Finder/",
+    },
+    {
+        img: "https://i.postimg.cc/gjFCpxBQ/Screenshot-2024-04-24-231745.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://naughty-cray-767d32.netlify.app/",
+    },
+    {
+        img: "https://i.postimg.cc/FHpNxVnR/Screenshot-2024-04-24-231931.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/Myntra/",
+    },
+    {
+        img: "https://i.postimg.cc/ZnmJcsZW/Screenshot-2024-04-24-232952.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/MajorProject/",
+    },
+    {
+        img: "https://i.postimg.cc/bYnBTN6C/Screenshot-2024-04-24-233741.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/Dynamic-Chart_with_Real-Time_Data/Interactive-Data-Visualization/",
+    },
+    {
+        img: "https://i.postimg.cc/7PNQcQk0/Screenshot-2024-04-24-234206.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://namishagurunani.github.io/Animation/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "namisha.gurunani173261@gmail.com",
+    YOUR_FONE: "8178945799",
+    description: "Let's connect and discuss how we can leverage technology to create impactful solutions together!",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/namishagurunani",
+    facebook: "https://www.facebook.com/profile.php?id=100090420699632",
+    linkedin: "https://www.linkedin.com/in/namisha-gurunani-82808025b",
+    twitter: "https://twitter.com/Namishagurunani",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
